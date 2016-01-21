8cea8e3548712fe52be643410fbaf3f806387b58
yhk1038 <yhk1038@hanmail.net>
2016-01-22 06:22:37 +0900

    use viewpager not comlete yet

--- app/src/main/java/com/example/user_/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user_/myapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.user_.myapplication;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,10 +15,27 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
 
+    int MAX_PAGE=3;                             //View Pager의 총 페이지 갯수를 나타내는 변수 선언
+    Fragment cur_fragment=new Fragment();       //현재 Viewpager가 가리키는 Fragment를 받을 변수 선언
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager=(ViewPager)findViewById(R.id.viewPager);        //Viewpager 선언 및 초기화
+        viewPager.setAdapter(new adapter(getSupportFragmentManager()));     //선언한 viewpager에 adapter를 연결
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.sliding_tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab One"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab Two"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab Three"));
+        tabLayout.setupWithViewPager(viewPager);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
@@ -43,4 +65,38 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+
+
+    private class adapter extends FragmentPagerAdapter {        //adapter클래스
+        public adapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            if(position<0 || MAX_PAGE<=position)        //가리키는 페이지가 0 이하거나 MAX_PAGE보다 많을 시 null로 리턴
+                return null;
+            switch (position){                          //포지션에 맞는 Fragment찾아서 cur_fragment변수에 대입
+                case 0:
+                    cur_fragment=new page_1();
+                    break;
+                case 1:
+                    cur_fragment=new page_2();
+                    break;
+                case 2:
+                    cur_fragment=new page_3();
+                    break;
+            }
+            return cur_fragment;
+        }
+        @Override
+        public int getCount() {
+            return MAX_PAGE;
+        }
+    }
+
 }
